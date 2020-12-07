3ec871d90dccc1c83704aed45bd001a3a82cb8cc
Shouta Yoshida <shota.yoshida.1003@gmail.com>
2020-12-07 23:36:07 +0900

    FB修正

--- common/footer.php
+++ common/footer.php
@@ -11,12 +11,10 @@
         <div class="col">
         </div>
     </div>
-    <div class="row d-block d-md-none">
-        <div class="col">
-            <a class="text-white f-16 ml-10" href="https://www.facebook.com/hoshirou.446" target="_blank"><i class="fab fa-facebook-square"></i></a>
-            <a class="text-white f-16 ml-20" href="https://twitter.com/hoshirou_" target="_blank"><i class="fab fa-twitter"></i></a>
-            <a class="text-white f-16 ml-20" href="https://www.instagram.com/hoshirou_/" target="_blank"><i class="fab fa-instagram"></i></a>
-            <a class="text-white f-16 ml-20" href="./contact.html" ><i class="fas fa-envelope-square"></i></a>
-        </div>
+    <div class="d-block d-md-none">
+        <a class="text-white f-16 ml-10" href="https://www.facebook.com/hoshirou.446" target="_blank"><i class="fab fa-facebook-square"></i></a>
+        <a class="text-white f-16 ml-20" href="https://twitter.com/hoshirou_" target="_blank"><i class="fab fa-twitter"></i></a>
+        <a class="text-white f-16 ml-20" href="https://www.instagram.com/hoshirou_/" target="_blank"><i class="fab fa-instagram"></i></a>
+        <a class="text-white f-16 ml-20" href="./contact.html" ><i class="fas fa-envelope-square"></i></a>
     </div>
 </footer>
