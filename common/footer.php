398aa78182842606f35365e4c31514d9ffa2574c
Shouta Yoshida <shota.yoshida.1003@gmail.com>
2020-12-08 23:05:59 +0900

    FB修正

--- common/footer.php
+++ common/footer.php
@@ -1,17 +1,5 @@
 <footer class="bg-dark line-h-2">
-    <div class="row d-none d-md-flex">
-        <div class="col">
-            <a class="text-white f-16 ml-20" href="https://www.facebook.com/hoshirou.446" target="_blank"><i class="fab fa-facebook-square"></i></a>
-	        <a class="text-white f-16 ml-20" href="https://twitter.com/hoshirou_" target="_blank"><i class="fab fa-twitter"></i></a>
-	        <a class="text-white f-16 ml-20" href="https://www.instagram.com/hoshirou_/" target="_blank"><i class="fab fa-instagram"></i></a>
-        </div>
-        <div class="col text-center">
-            <a class="text-white f-16 ml-10" href="./contact.html" ><i class="fas fa-envelope-square"></i></a>
-        </div>
-        <div class="col">
-        </div>
-    </div>
-    <div class="d-block d-md-none">
+    <div>
         <a class="text-white f-16 ml-10" href="https://www.facebook.com/hoshirou.446" target="_blank"><i class="fab fa-facebook-square"></i></a>
         <a class="text-white f-16 ml-20" href="https://twitter.com/hoshirou_" target="_blank"><i class="fab fa-twitter"></i></a>
         <a class="text-white f-16 ml-20" href="https://www.instagram.com/hoshirou_/" target="_blank"><i class="fab fa-instagram"></i></a>
--- common/nav.php
+++ common/nav.php
@@ -1,6 +1,5 @@
 <nav class="navbar navbar-expand-lg navbar-light bg-light">
-    <a class="navbar-brand " href="/"><img src="img/logo.gif" width="120px"></a>
-	<!--a class="navbar-brand roboto" href="/">hoshirou</a-->
+	<a class="navbar-brand roboto" href="/">hoshirou</a>
 
 	<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
 		<span class="navbar-toggler-icon"></span>
